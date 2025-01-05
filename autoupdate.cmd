811b13f159b0a8f9167cf32b2b1dae8c2af137ab
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-01-05 15:44:44 -0300

    Automated commit for Quantum-Teleporter version 0.0.1

---
+++ autoupdate.cmd
@@ -0,0 +1,111 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Carrega as variáveis do arquivo .env
+if exist .env (
+    for /f "tokens=* delims=" %%i in (.env) do (
+        set "%%i"
+    )
+)
+
+:: Verifica se a chave de API foi carregada
+if "%API_KEY%"=="" (
+    echo API_KEY=default_preset >> .env
+    echo Erro: A chave de API não foi carregada do arquivo .env.
+    exit /b 1
+)
+
+:: Leitura da versão a partir do arquivo info.json
+for /f "tokens=2 delims=:," %%a in ('type info.json ^| findstr /C:"\"version\""') do (
+    set "MOD_VERSION=%%~a"
+)
+
+:: Remove espaços em branco ao redor da versão
+set "MOD_VERSION=%MOD_VERSION: =%"
+set "MOD_VERSION=%MOD_VERSION:~1,-1%"
+
+:: Verifica se a versão foi obtida corretamente
+if "%MOD_VERSION%"=="" (
+    echo Erro: Falha ao obter a versão do arquivo info.json.
+    exit /b 1
+)
+
+:: Leitura do nome a partir do arquivo info.json
+for /f "tokens=2 delims=:," %%a in ('type info.json ^| findstr /C:"\"name\""') do (
+    set "MOD_NAME=%%~a"
+)
+
+:: Remove espaços em branco ao redor do nome
+set "MOD_NAME=%MOD_NAME: =%"
+set "MOD_NAME=%MOD_NAME:~1,-1%"
+
+:: Verifica se o nome foi obtido corretamente
+if "%MOD_NAME%"=="" (
+    echo Erro: Falha ao obter o nome do arquivo info.json.
+    exit /b 1
+)
+
+:: Configurações do script
+set STEAM_FILE="D:\SteamLibrary\steamapps\common\Factorio\mods"
+set AUTO_SEND=False
+set ZIP_FILE=%MOD_NAME%_%MOD_VERSION%.zip
+
+:: Remove qualquer arquivo ZIP anterior com o mesmo nome
+if exist "%ZIP_FILE%" (
+    del "%ZIP_FILE%"
+)
+
+:: Compacta todos os arquivos na pasta atual em um arquivo ZIP, excluindo .env e outros arquivos indesejados
+echo Compactando o mod em %ZIP_FILE%...
+tar -c -a -v -f "%ZIP_FILE%" --exclude=".env" --exclude="*.cmd" --exclude=".gitignore" --exclude="*.zip" --exclude="*.git" -o "."
+tar -c -a -v -f "%STEAM_FILE%/%ZIP_FILE%" --exclude=".env" --exclude="*.cmd" --exclude=".gitignore" --exclude="*.zip" --exclude="*.git" -o "."
+
+:: Verifica se o arquivo ZIP foi criado com sucesso
+if not exist "%ZIP_FILE%" (
+    echo Erro: Falha ao criar o arquivo ZIP.
+    exit /b 1
+)
+
+:: Inicializa o repositório Git (se necessário)
+if not exist ".git" (
+    git init
+    git remote add origin https://github.com/LUISDASARTIMANHAS/All-The-Mods-Translated-BR-Factorio-version.git
+)
+
+:: Verifica o status dos arquivos
+git status
+
+:: Adiciona todos os arquivos alterados, respeitando o .gitignore
+git add .
+
+:: Cria um commit com uma mensagem baseada no nome e versão do mod
+git commit -m "Automated commit for %MOD_NAME% version %MOD_VERSION%"
+
+:: Envia os arquivos para o repositório remoto na branch especificada
+git push origin main
+
+:: Remover espaços em branco ao redor de AUTO_SEND
+set "AUTO_SEND=%AUTO_SEND: =%"
+
+if /i "%AUTO_SEND%"=="False" (
+    echo Compactacao terminada. Auto publicar desativado. Saindo...
+    pause
+    exit /b 0
+)
+:: Publica o mod no Factorio
+echo Publicando o mod no Factorio...
+curl -X POST ^
+     -F "file=@%ZIP_FILE%" ^
+     -F "name=%MOD_NAME%" ^
+     -F "version=%MOD_VERSION%" ^
+     -H "Authorization: Token %API_KEY%" ^
+     https://mods.factorio.com/api/v2/mods/init_publish
+
+:: Verifica o código de retorno do cURL
+if errorlevel 1 (
+    echo Erro: Falha ao publicar o mod.
+    exit /b 1
+)
+
+echo Mod publicado com sucesso!
+exit /b 0
---
+++ data/blocos/quantum-teleporter-portal-entidade.lua
@@ -0,0 +1,158 @@
+local path_main = "__quantum-teleporter__/"
+
+data:extend({
+    {
+        type = "assembling-machine",
+        name = "quantum-teleporter-portal-entidade",
+        icon = path_main .. "graficos/blocos/quantum-teleporter-portal.png",
+        icon_size = 128,
+        icon_mipmaps = 4,
+        minable = { mining_time = 60, result = "quantum-teleporter-portal" },
+        max_health = 1500,
+        crafting_speed = 4,
+        ingredient_count = 3,
+        off_when_no_fluid_recipe = true,
+        energy_usage = "20MW",
+        buffer_capacity = "300MW",
+        crafting_categories = { "crafting" },
+        -- corpse = "big-remnants",
+        corpse = "assembling-machine-2-remnants",
+        -- dying_explosion = "big-explosion",
+        dying_explosion = "assembling-machine-2-explosion",
+        alert_icon_shift = { -0.09375, -0.375 },
+        collision_box = {
+            { -80, -80 },
+            { 100,  100 }
+        },
+        selection_box = {
+            { -80.5, -80.5 },
+            { 100.5,  100.5 }
+        },
+        drawing_box = {
+            { -80.5, -80.5 },
+            { 100.5,  100.5 }
+        },
+        energy_source = {
+            emissions_per_minute = 30,
+            type = "electric",
+            usage_priority = "secondary-input"
+        },
+        resistances = {
+            {
+                type = "fire",
+                percent = 70
+            },
+            {
+                type = "physical",
+                percent = 100
+            },
+            {
+                type = "impact",
+                percent = 100
+            },
+            {
+                type = "electric",
+                percent = 5
+            },
+            {
+                type = "explosion",
+                percent = 97
+            },
+            {
+                type = "laser",
+                percent = 100
+            }
+        },
+        damaged_trigger_effect = {
+            entity_name = "spark-explosion",
+            type = "create-entity",
+            damage_type_filters = "fire",
+            offsets = { { 0, 1 } },
+            offset_deviation = { { -0.5, -0.5 }, { 0.5, 0.5 } }
+        },
+        allowed_effects = {
+            "consumption",
+            "speed",
+            "productivity",
+            "pollution"
+        },
+        flags = {
+            "placeable-neutral",
+            "placeable-player",
+            "player-creation"
+        },
+        placeable_by = {
+            item = "4k-crafting-storage",
+            count = 1
+        },
+        module_specification = {
+            module_slots = 0,
+            module_info_icon_shift = { 0, 0.5 }
+        },
+        working_sound = {
+            sound = {
+                { filename = "__base__/sound/assembling-machine-t2-1.ogg", volume = 0.45 }
+            },
+            fade_in_ticks = 4,
+            audible_distance_modifier = 0.5,
+            fade_out_ticks = 20
+        },
+        vehicle_impact_sound = {
+            {
+                filename = "__base__/sound/car-metal-impact-2.ogg",
+                volume = 0.5
+            },
+            {
+                filename = "__base__/sound/car-metal-impact-3.ogg",
+                volume = 0.5
+            },
+            {
+                filename = "__base__/sound/car-metal-impact-4.ogg",
+                volume = 0.5
+            },
+            {
+                filename = "__base__/sound/car-metal-impact-5.ogg",
+                volume = 0.5
+            },
+            {
+                filename = "__base__/sound/car-metal-impact-6.ogg",
+                volume = 0.5
+            }
+        },
+        animation = {
+            layers = {
+                {
+                    frame_count = 1,
+                    filename = path_main .. "graficos/blocos/4k_crafting_storage.png",
+                    hr_version = {
+                        frame_count = 1,
+                        filename = "__base__/graphics/entity/assembling-machine-2/hr-assembling-machine-2.png",
+                        height = 128,
+                        line_length = 1,
+                        shift = { 0, 0.125 },
+                        scale = 0.5,
+                        priority = "high",
+                        width = 128
+                    },
+                    height = 128,
+                    line_length = 1,
+                    shift = { 0, 0.125 },
+                    priority = "high",
+                    width = 128
+                }
+            }
+        },
+        close_sound = {
+            {
+                filename = "__base__/sound/machine-close.ogg",
+                volume = 0.5
+            }
+        },
+        open_sound = {
+            {
+                filename = "__base__/sound/machine-open.ogg",
+                volume = 0.5
+            }
+        },
+    }
+})
